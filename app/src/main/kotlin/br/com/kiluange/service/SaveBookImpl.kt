852fcb556f5abfb80955561b836f628e098f2ee5
Daniel Kiluange <daniel.kiluange@gmail.com>
2022-06-23 17:52:06 -0300

    add book builder

--- app/src/main/kotlin/br/com/kiluange/service/SaveBookImpl.kt
+++ app/src/main/kotlin/br/com/kiluange/service/SaveBookImpl.kt
@@ -2,10 +2,11 @@ package br.com.kiluange.service
 
 import br.com.kiluange.Book
 import br.com.kiluange.usecase.SaveBook
-import java.util.*
 
 class SaveBookImpl : SaveBook {
     override suspend fun saveBook(name: String): Book {
-        return Book(UUID.randomUUID(), name)
+        val builder: Book.Builder = Book.Builder("Teste")
+            .category("não categorizado")
+        return builder.build()
     }
 }
--- data/src/main/kotlin/br/com/kiluange/Book.kt
+++ data/src/main/kotlin/br/com/kiluange/Book.kt
@@ -2,4 +2,23 @@ package br.com.kiluange
 
 import java.util.*
 
-data class Book(val id: UUID, val name: String)
+data class Book(val id: UUID, val name: String, val categories: List<String> = mutableListOf()) {
+
+    class Builder(var name: String, var categories: MutableList<String> = mutableListOf()) {
+
+        fun categories(value: MutableList<String>): Builder {
+            this.categories = value
+            return this
+        }
+
+        fun category(value: String): Builder {
+            this.categories.add(value)
+            return this
+        }
+
+        fun build() = Book(UUID.randomUUID(), name, categories)
+    }
+
+    fun toBuilder() = Builder(this.name, this.categories.toMutableList())
+
+}
